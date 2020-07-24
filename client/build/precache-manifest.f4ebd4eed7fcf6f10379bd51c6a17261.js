a8df6b824dd0810440e29443ce97e1651b652010
Alayne Theodore <alaynetheodore@Alaynes-MacBook-Pro.local>
2020-07-24 11:27:18 -0500

    added build

---
+++ client/build/precache-manifest.f4ebd4eed7fcf6f10379bd51c6a17261.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1326bf357d954eb2b81393df31612bbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bab3f412032c0df69c4f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "45d08be249103a540abd",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "bab3f412032c0df69c4f",
+    "url": "/static/js/2.85cac07b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.85cac07b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d08be249103a540abd",
+    "url": "/static/js/main.42e5d48f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
